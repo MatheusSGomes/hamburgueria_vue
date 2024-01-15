cf3d688317718b2a8a297d18bc3abf4f610e4d64
Matheus Gomes <math.s.gomes@gmail.com>
2024-01-15 18:59:01 -0300

    reset css e estilização header

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">
-      <img src="" id="logo">
+  <nav class="navbar">
+    <router-link to="/" class="logo-url">
+      <img :src="logo" :alt="alt" class="logo">
     </router-link>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/pedidos">Pedidos</router-link>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'NavbarComponent'
+  name: 'NavbarComponent',
+  props: ['logo', 'alt']
 }
 </script>
 
-<style>
+<style scoped>
+  .navbar {
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
 
+  .navbar .logo-url {
+    margin: auto;
+    margin-left: 0;
+  }
+
+  .logo {
+    width: 40px;
+    height: 40px;
+  }
+
+  .navbar a {
+    color: #FCBA03;
+    text-decoration: none;
+    margin: 12px;
+    transition: .5s;
+  }
+
+  nav a:hover {
+    color: #FFF;
+  }
 </style>
