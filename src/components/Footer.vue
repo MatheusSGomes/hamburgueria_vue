7a29ca586185e54957faead28cf57b1171e4bced
Matheus Gomes <math.s.gomes@gmail.com>
2024-01-15 19:13:49 -0300

    estilização footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>Footer</p>
+  <footer class="footer">
+    <p>Hamburgueria &copy; 2024</p>
+  </footer>
 </template>
 
 <script>
@@ -9,5 +11,15 @@ export default {
 </script>
 
 <style scoped>
-
+  .footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+  }
 </style>
